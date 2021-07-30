9da6ed72dc8543bbba747978200469923c701bf6
ans2552 <hja5432@gmail.com>
2021-07-30 20:30:46 +0900

    1037

---
+++ problems/1037.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int arr[50];
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	cout << arr[0] * arr[N - 1];	
+}
