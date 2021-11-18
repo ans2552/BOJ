e7876df947f6dde11e4254a782de91a013ff3ab0
ans2552 <hja5432@gmail.com>
2021-11-18 20:50:13 +0900

    2693

---
+++ problems/2693.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[10];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	while (n--) {
+		for (int i = 0; i < 10; i++) {
+			cin >> arr[i];
+		}
+		sort(arr, arr + 10);
+
+		cout << arr[7] << '\n';
+	}
+}
