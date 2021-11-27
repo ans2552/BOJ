f8b36763e82dce809c56605ab3978e4322c07fef
ans2552 <hja5432@gmail.com>
2021-11-27 14:56:36 +0900

    9237

---
+++ problems/9237.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int N;
+int arr[100000];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    int answer = 0;
+    for (int i = 0; i < N; i++) {
+        answer = max(answer, arr[N - 1 - i] + i);
+    }
+    cout << answer + 2;
+}
